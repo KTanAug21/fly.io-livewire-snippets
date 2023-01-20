837c4dd4b5c357b38fad36ab8e17eb36b127c25b
Kathryn <ktan.software.developer@gmail.com>
2023-01-20 23:22:58 +0300

    changes

--- app/Http/Controllers/DocumentCrudController.php
+++ app/Http/Controllers/DocumentCrudController.php
@@ -18,9 +18,43 @@ class DocumentCrudController extends Controller
      * This controller is under construction.
      * Read at your own peace of mind. 
      */
+
+    public function deleteAll()
+    {
+        // Delete all files in uploads directory
+        $files = Storage::allFiles('storage/uploads');
+        Storage::delete($files);
+        /*foreach( $files as $file ){
+            if( $file != 'storage/uploads/SampleDocument.pdf' )
+                Storage::delete($file);
+        }*/
+       
+        // Delete all files here
+        // Identify Primary to replay to
+        $lfHelper = new \App\Helpers\LiteFSHelper();
+   
+        // Decide replay
+        if( $lfHelper->forwardWriteRequest() ){     
+            Log::info('forwarding to primary...');   
+            $replayTo = $lfHelper->getPrimaryNodeId();
+            // Replay to identified
+            return response('', 200, [
+                'fly-replay' => "instance=$replayTo",
+            ]);
+        }else{
+            Log::info('DB deleting all for documents table in '.env('FLY_REGION') );
+
+            Document::truncate();
+            return redirect('documents');
+        }
+
+    }
+    
+  
     
     public function uploadFra(Request $request)
     {
+        Log::info('Here\'s my region:'. env('FLY_REGION'));
         // Validation
         $validated = $request->validate([
             'pdf' => [
@@ -33,9 +67,9 @@ class DocumentCrudController extends Controller
         $originalName = $file->getClientOriginalName();
 
         // Straight save to db in the ams region ( primary ) 
-        // File Upload in the fra regopm
         if( env('FLY_REGION') == 'ams' ){ 
 
+            Log::info('Creatging record');
             Document::create([
                 'full_path'   => 'storage/uploads/'.$originalName,
                 'region_id'   => 'fra' // File is stored in the accepting instance, so we better use this as the region of the instance saving the file
@@ -50,6 +84,9 @@ class DocumentCrudController extends Controller
                 'storage/uploads',
                 $originalName
             );
+            return response('', 200, [
+                'ok' => "ok",
+            ]);
         }else{
             Log::info('forwarding to fra...');   
             // Replay to identified
