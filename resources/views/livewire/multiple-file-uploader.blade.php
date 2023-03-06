97c3fc124e482bc10552f047f1368729bfa3220e
Kathryn <ktan.software.developer@gmail.com>
2023-03-06 14:38:19 +0300

    clean view of test snippets

--- resources/views/livewire/multiple-file-uploader.blade.php
+++ resources/views/livewire/multiple-file-uploader.blade.php
@@ -40,44 +40,6 @@
                 }
             });
         }
-        /*filesSelector.addEventListener('change', () => {
-            const fileList = [...filesSelector.files];
-            fileList.forEach((file, index) => {
-                @this.set('uploads.'+index+'.fileName', file.name );
-                @this.set('uploads.'+index+'.fileSize', file.size );
-                @this.set('uploads.'+index+'.progress', 0 );
-                
-                chunkStarts[index] = 0;
-                livewireUploadChunk( index, file );
-            });
-        });
-
-        function livewireUploadChunk( index, file ){
-            
-            // Get the chunk
-            const chunkEnd = Math.min( chunkStarts[index] + @js($chunkSize), file.size );
-            var date       = Math.floor(Date.now() / 1000);
-            const chunk    = file.slice( chunkStarts[index], chunkEnd );
-
-            @this.upload('uploads.'+index+'.fileChunk',chunk,(n)=>{},()=>{},(e)=>{
-                if( e.detail.progress == 100 ){
-
-                    console.log(
-                        file.name, ' prev value is ', chunkStarts[index], 
-                        '; const:',startCnst,
-                        '; let:', startLt, 
-                        '; var:', startVar, 
-                    ' ', date);
-
-                    chunkStarts[index] = Math.min( startCnst + @js($chunkSize), file.size );
-                    
-                    if( chunkStarts[index] < file.size ){
-                        console.log( 'calling upload for ', file.name, 'start at ',startCnst, ' waiting at  ms on date ', date );
-                        livewireUploadChunk( index, file );
-                    }
-                }
-            });
-        };*/
 
     </script>
 </div>
